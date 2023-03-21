30ac2ed12b434f994ec2fc461b1a8b34c2e00681
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-22 00:09:09 +0700

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Grid } from '@mui/material';
 import ProductCard from './ProductCard';
 
-function ProductList({ products, loading }) {
+function ProductList({ products }) {
   return (
     <Grid container spacing={2} mt={1}>
-      {products.map((product, index) => (
+      {products.map((product) => (
         <Grid key={product.id} item xs={6} md={4} lg={3}>
           <ProductCard product={product} />
         </Grid>
