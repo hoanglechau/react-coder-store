026515f0a9309683aabf41b5cfb1a327cf186a8d
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:12:15 +0700

    Update AuthRequire.js

--- src/routes/AuthRequire.js
+++ src/routes/AuthRequire.js
@@ -1,20 +1,21 @@
-import { Navigate, useLocation } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
-import LoadingScreen from "../components/LoadingScreen";
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import LoadingScreen from '../components/LoadingScreen';
 
 function AuthRequire({ children }) {
-    const { isAuthenticated, isInitialized } = useAuth();
-    const location = useLocation();
+  const { isAuthenticated, isInitialized } = useAuth();
+  const location = useLocation();
 
-    if (!isInitialized) {
-        return <LoadingScreen />;
-    }
+  if (!isInitialized) {
+    return <LoadingScreen />;
+  }
 
-    if (!isAuthenticated) {
-        return <Navigate to="/login" state={{ from: location }} replace />;
-    }
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-    return children;
+  return children;
 }
 
 export default AuthRequire;
