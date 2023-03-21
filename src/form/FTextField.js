0148b32c478ec17578b4991593568428d60dde34
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:11:59 +0700

    Update FTextField.js

--- src/form/FTextField.js
+++ src/form/FTextField.js
@@ -1,24 +1,26 @@
-import { useFormContext, Controller } from "react-hook-form";
-import { TextField } from "@mui/material";
+import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { TextField } from '@mui/material';
 
 function FTextField({ name, ...other }) {
-    const { control } = useFormContext();
+  const { control } = useFormContext();
 
-    return (
-        <Controller
-            name={name}
-            control={control}
-            render={({ field, fieldState: { error } }) => (
-                <TextField
-                    {...field}
-                    fullWidth
-                    error={!!error}
-                    helperText={error?.message}
-                    {...other}
-                />
-            )}
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {...field}
+          fullWidth
+          error={!!error}
+          helperText={error?.message}
+          {...other}
         />
-    );
+      )}
+    />
+  );
 }
 
 export default FTextField;
