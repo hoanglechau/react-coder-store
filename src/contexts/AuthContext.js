7780cd1da609fb836e07e3f50883c267fdce88ef
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:11:43 +0700

    Update AuthContext.js

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,103 +1,104 @@
-import { createContext, useReducer, useEffect } from "react";
+import React, { createContext, useReducer, useEffect } from 'react';
 
 const initialState = {
-    isAuthenticated: false,
-    isInitialized: false,
-    user: null,
+  isAuthenticated: false,
+  isInitialized: false,
+  user: null,
 };
 
-const INITIALIZE = "INITIALIZE";
-const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-const LOGOUT = "LOGOUT";
+const INITIALIZE = 'INITIALIZE';
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+const LOGOUT = 'LOGOUT';
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case INITIALIZE:
-            const { isAuthenticated, user } = action.payload;
-            return {
-                ...state,
-                isAuthenticated,
-                isInitialized: true,
-                user,
-            };
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                isAuthenticated: true,
-                user: action.payload.user,
-            };
-        case LOGOUT:
-            return {
-                ...state,
-                isAuthenticated: false,
-                user: null,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case INITIALIZE:
+      // eslint-disable-next-line no-case-declarations
+      const { isAuthenticated, user } = action.payload;
+      return {
+        ...state,
+        isAuthenticated,
+        isInitialized: true,
+        user,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
 };
 
 const AuthContext = createContext({ ...initialState });
 
 function AuthProvider({ children }) {
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    useEffect(() => {
-        const initialize = async () => {
-            try {
-                const username = window.localStorage.getItem("username");
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const username = window.localStorage.getItem('username');
 
-                if (username) {
-                    dispatch({
-                        type: INITIALIZE,
-                        payload: { isAuthenticated: true, user: { username } },
-                    });
-                } else {
-                    dispatch({
-                        type: INITIALIZE,
-                        payload: { isAuthenticated: false, user: null },
-                    });
-                }
-            } catch (err) {
-                console.error(err);
-                dispatch({
-                    type: INITIALIZE,
-                    payload: {
-                        isAuthenticated: false,
-                        user: null,
-                    },
-                });
-            }
-        };
-        initialize();
-    }, []);
-
-    const login = async (username, callback) => {
-        window.localStorage.setItem("username", username);
+        if (username) {
+          dispatch({
+            type: INITIALIZE,
+            payload: { isAuthenticated: true, user: { username } },
+          });
+        } else {
+          dispatch({
+            type: INITIALIZE,
+            payload: { isAuthenticated: false, user: null },
+          });
+        }
+      } catch (err) {
+        console.error(err);
         dispatch({
-            type: LOGIN_SUCCESS,
-            payload: { user: { username } },
+          type: INITIALIZE,
+          payload: {
+            isAuthenticated: false,
+            user: null,
+          },
         });
-        callback();
+      }
     };
+    initialize();
+  }, []);
 
-    const logout = async (callback) => {
-        window.localStorage.removeItem("username");
-        dispatch({ type: LOGOUT });
-        callback();
-    };
+  const login = async (username, callback) => {
+    window.localStorage.setItem('username', username);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: { user: { username } },
+    });
+    callback();
+  };
 
-    return (
-        <AuthContext.Provider
-            value={{
-                ...state,
-                login,
-                logout,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  const logout = async (callback) => {
+    window.localStorage.removeItem('username');
+    dispatch({ type: LOGOUT });
+    callback();
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export { AuthContext, AuthProvider };
