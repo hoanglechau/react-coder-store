2a49c6dfee3b8c1ec90eb23c5fcaadec4a63af02
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:12:10 +0700

    Update MainLayout.js

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,20 +1,20 @@
-import { Outlet } from "react-router-dom";
-import { Box, Stack } from "@mui/material";
-import MainFooter from "./MainFooter";
-import MainHeader from "./MainHeader";
+import { Outlet } from 'react-router-dom';
+import { Box, Stack } from '@mui/material';
+import MainFooter from './MainFooter';
+import MainHeader from './MainHeader';
 
 function MainLayout() {
-    return (
-        <Stack sx={{ minHeight: "100vh" }}>
-            <MainHeader />
+  return (
+    <Stack sx={{ minHeight: '100vh' }}>
+      <MainHeader />
 
-            <Outlet />
+      <Outlet />
 
-            <Box sx={{ flexGrow: 1 }} />
+      <Box sx={{ flexGrow: 1 }} />
 
-            <MainFooter />
-        </Stack>
-    );
+      <MainFooter />
+    </Stack>
+  );
 }
 
 export default MainLayout;
