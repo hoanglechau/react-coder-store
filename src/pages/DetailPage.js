793b2516c3c26c6be265a80d4998cb29520ffa7d
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:12:12 +0700

    Update DetailPage.js

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -61,96 +61,96 @@ function DetailPage() {
             ) : (
               <>
                 {product && (
-                    <Card>
-                      <Grid container>
-                            <Grid item xs={12} md={6}>
-                              <Box p={2}>
-                                <Box
-                                      sx={{
-                                        borderRadius: 2,
-                                        overflow: 'hidden',
-                                        display: 'flex',
-                                      }}
-                                    >
-                                      <Box
-                                        component="img"
-                                        sx={{
-                                          width: 1,
-                                          height: 1,
-                                        }}
-                                        src={product.cover}
-                                        alt="product"
-                                      />
-                                    </Box>
-                              </Box>
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                              <Typography
-                                variant="h6"
-                                sx={{
-                                      mt: 2,
-                                      mb: 1,
-                                      display: 'block',
-                                      textTransform: 'uppercase',
-                                      color:
+                <Card>
+                  <Grid container>
+                    <Grid item xs={12} md={6}>
+                      <Box p={2}>
+                        <Box
+                          sx={{
+                            borderRadius: 2,
+                            overflow: 'hidden',
+                            display: 'flex',
+                          }}
+                        >
+                          <Box
+                            component="img"
+                            sx={{
+                              width: 1,
+                              height: 1,
+                            }}
+                            src={product.cover}
+                            alt="product"
+                          />
+                        </Box>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          mt: 2,
+                          mb: 1,
+                          display: 'block',
+                          textTransform: 'uppercase',
+                          color:
                                                             product.status === 'sale'
                                                               ? 'error.main'
                                                               : 'info.main',
-                                    }}
-                              >
-                                {product.status}
-                              </Typography>
-                              <Typography variant="h5" paragraph>
-                                {product.name}
-                              </Typography>
-                              <Stack
-                                direction="row"
-                                alignItems="center"
-                                spacing={1}
-                                sx={{ mb: 2 }}
-                              >
-                                <Rating
-                                      value={product.totalRating}
-                                      precision={0.1}
-                                      readOnly
-                                    />
-                                <Typography
-                                      variant="body2"
-                                      sx={{ color: 'text.secondary' }}
-                                    >
-                                      (
-                                      {product.totalReview}
-                                      {' '}
-                                      reviews)
-                                    </Typography>
-                              </Stack>
-                              <Typography variant="h4" sx={{ mb: 3 }}>
-                                <Box
-                                      component="span"
-                                      sx={{
-                                        color: 'text.disabled',
-                                        textDecoration: 'line-through',
-                                      }}
-                                    >
-                                      {product.priceSale && fCurrency(product.priceSale)}
-                                    </Box>
-                                {fCurrency(product.price)}
-                              </Typography>
+                        }}
+                      >
+                        {product.status}
+                      </Typography>
+                      <Typography variant="h5" paragraph>
+                        {product.name}
+                      </Typography>
+                      <Stack
+                        direction="row"
+                        alignItems="center"
+                        spacing={1}
+                        sx={{ mb: 2 }}
+                      >
+                        <Rating
+                          value={product.totalRating}
+                          precision={0.1}
+                          readOnly
+                        />
+                        <Typography
+                          variant="body2"
+                          sx={{ color: 'text.secondary' }}
+                        >
+                          (
+                          {product.totalReview}
+                          {' '}
+                          reviews)
+                        </Typography>
+                      </Stack>
+                      <Typography variant="h4" sx={{ mb: 3 }}>
+                        <Box
+                          component="span"
+                          sx={{
+                            color: 'text.disabled',
+                            textDecoration: 'line-through',
+                          }}
+                        >
+                          {product.priceSale && fCurrency(product.priceSale)}
+                        </Box>
+                        {fCurrency(product.price)}
+                      </Typography>
 
-                              <Divider sx={{ borderStyle: 'dashed' }} />
-                              <Box>
-                                <ReactMarkdown
-                                      rehypePlugins={[rehypeRaw]}
-                                      children={product.description}
-                                    />
-                              </Box>
-                            </Grid>
-                          </Grid>
-                    </Card>
-                  )}
+                      <Divider sx={{ borderStyle: 'dashed' }} />
+                      <Box>
+                        <ReactMarkdown
+                          rehypePlugins={[rehypeRaw]}
+                          children={product.description}
+                        />
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Card>
+                )}
                 {!product && (
-                    <Typography variant="h6">404 Product not found</Typography>
-                  )}
+                <Typography variant="h6">404 Product not found</Typography>
+                )}
               </>
             )}
           </>
