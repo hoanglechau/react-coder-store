b05114f8f10c30cfcfdf20b00c61e593c4490b21
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:11:49 +0700

    Update FMultiCheckbox.js

--- src/form/FMultiCheckbox.js
+++ src/form/FMultiCheckbox.js
@@ -1,39 +1,39 @@
-import { useFormContext, Controller } from "react-hook-form";
-import { Checkbox, FormGroup, FormControlLabel } from "@mui/material";
+import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { Checkbox, FormGroup, FormControlLabel } from '@mui/material';
 
 function FMultiCheckbox({ name, options, ...other }) {
-    const { control } = useFormContext();
+  const { control } = useFormContext();
 
-    return (
-        <Controller
-            name={name}
-            control={control}
-            render={({ field }) => {
-                const onSelected = (option) =>
-                    field.value.includes(option)
-                        ? field.value.filter((value) => value !== option)
-                        : [...field.value, option];
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => {
+        const onSelected = (option) => (field.value.includes(option)
+          ? field.value.filter((value) => value !== option)
+          : [...field.value, option]);
 
-                return (
-                    <FormGroup>
-                        {options.map((option) => (
-                            <FormControlLabel
-                                key={option}
-                                control={
-                                    <Checkbox
-                                        checked={field.value.includes(option)}
-                                        onChange={() => field.onChange(onSelected(option))}
-                                    />
-                                }
-                                label={option}
-                                {...other}
-                            />
-                        ))}
-                    </FormGroup>
-                );
-            }}
-        />
-    );
+        return (
+          <FormGroup>
+            {options.map((option) => (
+              <FormControlLabel
+                key={option}
+                control={(
+                  <Checkbox
+                    checked={field.value.includes(option)}
+                    onChange={() => field.onChange(onSelected(option))}
+                  />
+                                  )}
+                label={option}
+                {...other}
+              />
+            ))}
+          </FormGroup>
+        );
+      }}
+    />
+  );
 }
 
 export default FMultiCheckbox;
