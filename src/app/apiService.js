7b7433d6f8a89b9b686e1207539e5e93482fe6de
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:11:35 +0700

    Update apiService.js

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,30 +1,30 @@
-import axios from "axios";
-import { BASE_URL } from "./config";
+import axios from 'axios';
+import { BASE_URL } from './config';
 
 const apiService = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 apiService.interceptors.request.use(
-    (request) => {
-        console.log("Start Request", request);
-        return request;
-    },
-    function (error) {
-        console.log("REQUEST ERROR", error);
-        return Promise.reject(error);
-    }
+  (request) => {
+    console.log('Start Request', request);
+    return request;
+  },
+  (error) => {
+    console.log('REQUEST ERROR', error);
+    return Promise.reject(error);
+  },
 );
 
 apiService.interceptors.response.use(
-    (response) => {
-        console.log("Response", response);
-        return response;
-    },
-    function (error) {
-        console.log("RESPONSE ERROR", error);
-        return Promise.reject(error);
-    }
+  (response) => {
+    console.log('Response', response);
+    return response;
+  },
+  (error) => {
+    console.log('RESPONSE ERROR', error);
+    return Promise.reject(error);
+  },
 );
 
 export default apiService;
