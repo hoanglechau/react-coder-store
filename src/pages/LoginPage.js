7068891fe21a88b4d0b30de698caa1e4fae7e7e8
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 15:10:19 +0700

    Create LoginPage.js

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,53 @@
+import { Button, Stack, Typography } from '@mui/material';
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import useAuth from '../hooks/useAuth';
+import { FormProvider, FTextField } from '../components/form';
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string().required('Username is required'),
+});
+const defaultValues = {
+  username: '',
+};
+
+function LoginPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+  const { handleSubmit } = methods;
+
+  const onSubmit = async (data) => {
+    const from = location.state?.from?.pathname || '/';
+    const { username } = data;
+
+    auth.login(username, () => {
+      navigate(from, { replace: true });
+    });
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3} sx={{ minWidth: '350px' }}>
+        <Typography variant="h4" textAlign="center">
+          Login
+        </Typography>
+        <FTextField name="username" label="Username" />
+
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+      </Stack>
+    </FormProvider>
+  );
+}
+
+export default LoginPage;
