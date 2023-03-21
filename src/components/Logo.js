07e85f410db64bc787939bcbab636c6c05134960
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 22:11:41 +0700

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,19 +1,19 @@
-import { Link as RouterLink } from "react-router-dom";
-import { Box } from "@mui/material";
-import logoImg from "../logo.png";
+import { Link as RouterLink } from 'react-router-dom';
+import { Box } from '@mui/material';
+import logoImg from '../logo.png';
 
 function Logo({ disabledLink = false, sx }) {
-    const logo = (
-        <Box sx={{ width: 40, height: 40, ...sx }}>
-            <img src={logoImg} alt="logo" width="100%" />
-        </Box>
-    );
+  const logo = (
+    <Box sx={{ width: 40, height: 40, ...sx }}>
+      <img src={logoImg} alt="logo" width="100%" />
+    </Box>
+  );
 
-    if (disabledLink) {
-        return <>{logo}</>;
-    }
+  if (disabledLink) {
+    return <>{logo}</>;
+  }
 
-    return <RouterLink to="/">{logo}</RouterLink>;
+  return <RouterLink to="/">{logo}</RouterLink>;
 }
 
 export default Logo;
